55c73d4f5413de124780463edaea63618ba26634
emanuel amaury fuentes venegas <fuentesamaury@hotmail.com>
2019-05-29 21:54:05 -0500

    monitor funcionando, en espera de pruebas con OIDS

--- src/main/java/Bsnmp/Monitor.java
+++ src/main/java/Bsnmp/Monitor.java
@@ -1,7 +1,11 @@
 package Bsnmp;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
 import java.net.SocketException;
+import java.net.UnknownHostException;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -24,14 +28,28 @@ public class Monitor extends Thread
     final static Logger logger = LogManager.getLogger(Monitor.class);
     final String ADMON_MAIL = "fuentesamaury@hotmail.com";
     final String IP_MAIL = "localhost";
-    final String PORT_MAIL = "465";
+    final int PORT_MAIL = 2020;
 
+    private String nameDevice = "";
     private Address address = GenericAddress.parse("udp:127.0.0.1/161");
     private int onCheckObjects = 3; // maximos objetos escaneados
     private int alertCpu = 80; // DEFAULT ALERT GRADE ON %
     private int alertDisk = 80; // DEFAULT ALERT GRADE ON %
+    private int timeForNotify = 60000; // tiempo de espera antes de enviar una alerta nuevamente
+    private int timeFor = 0; // a que dispositivo se le aplicara el tiempo de espera
+    String connInfo = "";
+    String memInfo = "";
+    String diskInfo = "";
+
     private TransportMapping tm; // = new DefaultUdpTransportMapping();
     private Snmp sn;
+    private Thread[] timer = new TimerComponentes[3]; // EN ORDE
+
+    private enum timeClass {
+        MEMORY,
+        DISK,
+        CONNECTION;
+    }
 
     private enum estado {
         ESTABLE,
@@ -40,11 +58,16 @@ public class Monitor extends Thread
         ERROR,
     }
 
+    private estado state = estado.ESTABLE;
+
     private enum errorCode {
+        EXITO,
         UDP_ERROR_INSTANCE,
         BAD_DATA_READER,
         BAD_DATA_INCOMING,
     }
+
+    private errorCode eCode = errorCode.EXITO;
 
     /***
      * Inicia el monitoreo de la instancia seleccionada proporcionando el porcentaje
@@ -55,21 +78,38 @@ public class Monitor extends Thread
      * @param alertDisk
      * @throws IOException
      */
-    public Monitor(String address, int alertCpu, int alertDisk) throws IOException
+    public Monitor(String nameDevice, String address, int alertCpu, int alertDisk)
     {
+        this.nameDevice = nameDevice;
         this.alertCpu = alertCpu;
         this.alertDisk = alertDisk;
-        this.address = GenericAddress.parse("upd:" + address + "/161");
+        this.address = GenericAddress.parse("udp:" + address + "/161");
+        logger.info("Generando conexion SNMP hacia " + this.address);
+        this.address = GenericAddress.parse(address);
         try
         {
-            tm = new DefaultUdpTransportMapping();
+            init();
         }
         catch (SocketException ex)
         {
             ex.printStackTrace();
             logger.error("Error - Falla en protocolo UDP ");
         }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            logger.error("Error - Falla en protocolo UDP ");
+        }
+    }
+
+    public void init() throws IOException
+    {
+        tm = new DefaultUdpTransportMapping();
         sn = new Snmp(tm);
+        for (int i = 0; i < timer.length; i++)
+        {
+            timer[i] = new TimerComponentes(timeForNotify);
+        }
     }
 
     /***
@@ -79,8 +119,9 @@ public class Monitor extends Thread
      * @param address
      * @throws IOException
      */
-    public Monitor(String address) throws IOException
+    public Monitor(String nameDevice, String address) throws IOException
     {
+        this.nameDevice = nameDevice;
         this.address = GenericAddress.parse("upd:" + address + "/161");
         try
         {
@@ -102,7 +143,11 @@ public class Monitor extends Thread
             tm.listen();
             secuencialMonitoring();
         }
-
+        catch (RuntimeException e)
+        {
+            e.printStackTrace();
+            logger.error("Error - Agente no iniciado");
+        }
         catch (InterruptedException e)
         {
             e.printStackTrace();
@@ -122,25 +167,21 @@ public class Monitor extends Thread
      * @throws InterruptedException
      * @throws NullPointerException
      */
-    public void secuencialMonitoring() throws IOException, InterruptedException, NullPointerException
+    private void secuencialMonitoring() throws IOException, InterruptedException, RuntimeException
     {
         boolean alive = true;
         while (alive)
         {
-            // GENERIC METHOD OIDs
-            // Windows and Generics hrProcessorLoad 1.3.6.1.2.1.25.3.3.1.2
-            // Windows and Generics hrSWRunPerfMem 1.3.6.1.2.1.25.5.1.1.2
-            // Windows and Generics N.Memory.SNMP.HrStorage 1.3.6.1.2.1.25.2.3.1.2
-            String sysInfo3 = getAsString(new OID(".1.3.6.1.4.1.2021.4.5"));
-            logger.info("Monitoreo SpaceFreeOnDisk :::  " + sysInfo3);
-            String sysInfo2 = getAsString(new OID(".1.3.6.1.4.1.2021.4.5"));
-            logger.info("Monitoreo SpaceFreeOnDisk :::  " + sysInfo2);
-            String sysInfo = getAsString(new OID(".1.3.6.1.4.1.2021.4.11.0"));
-            logger.info("Monitoreo MemoryFree Kb :::  " + sysInfo);
+
+            memInfo = getAsString(new OID("1.3.6.1.4.1.2021.4.11.0"));
+            logger.debug("Monitoreo MemoryFree Kb :::  " + memInfo);
+            diskInfo = getAsString(new OID(".1.3.6.1.4.1.2021.9.1.7.1"));
+            logger.debug("Monitoreo SpaceFreeOnDisk :::  " + diskInfo);
+            connInfo = getAsString(new OID(".1.3.6.1.4.1.2021.4.11.0"));
+            logger.debug("Monitoreo ConnectionDB :::  " + connInfo);
+
             Thread.sleep(5000);
-            String data1 = "";
-            String data2 = "";
-            analyzeData(data1, data2);
+            analyzeData(memInfo, diskInfo, connInfo);
         }
     }
 
@@ -152,14 +193,16 @@ public class Monitor extends Thread
      * @param dataMemory
      * @param diskMemory
      */
-    public void analyzeData(String dataMemory, String diskMemory)
+    private void analyzeData(String dataMemory, String diskMemory, String connDB)
     {
         int[] analyzerArray = new int[3]; // Storage for data analyzer
         try
         {
-            analyzerArray[0] = Integer.parseInt("1");
-            analyzerArray[1] = Integer.parseInt("0");
-            analyzerArray[2] = Integer.parseInt("2");
+
+            analyzerArray[0] = Integer.parseInt("100");
+            analyzerArray[1] = Integer.parseInt("100");
+            analyzerArray[2] = Integer.parseInt("100");
+
         }
         catch (NumberFormatException e)
         {
@@ -172,21 +215,27 @@ public class Monitor extends Thread
             switch (i)
             {
             case 0:
-                if (analyzerArray[0] < (alertCpu - 100))
+                if (analyzerArray[0] < (alertCpu - 100) && !timer[0].isAlive())
                 {
-
+                    logger.info("ALERTA - LA MEMORIA RAM DEL DISPOSITIVO ESTA BAJA.");
+                    timeFor = timeClass.MEMORY.ordinal();
+                    timer[timeFor].start();
                 }
                 break;
             case 1:
-                if (analyzerArray[1] < (alertDisk - 100))
+                if (analyzerArray[1] < (alertDisk - 100) && !timer[1].isAlive())
                 {
-
+                    logger.info("ALERTA - EL DISCO ESTA CASI LLENO, ASEGURESE DE LIBERAR ESPACIO.");
+                    timeFor = timeClass.DISK.ordinal();
+                    timer[timeFor].start();
                 }
                 break;
             case 2:
-                if (analyzerArray[2] < (alertCpu - 100))
+                if (analyzerArray[2] == -1 && !timer[2].isAlive()) // -1 INDICA, DESCONEXION
                 {
-
+                    logger.info("Conexion perdida con la base de datos, favor de reestablecerla.");
+                    timeFor = timeClass.CONNECTION.ordinal();
+                    timer[timeFor].start();
                 }
                 break;
             default:
@@ -195,13 +244,59 @@ public class Monitor extends Thread
         }
     }
 
-    public String getAsString(OID oid) throws IOException
+    private void alertaEmail(int msgCode)
+    {
+        logger.info("Enviando solicitud del cliente.");
+        Socket clientSocket = null;
+        try
+        {
+            clientSocket = new Socket(IP_MAIL, PORT_MAIL);
+            OutputStream outStream = clientSocket.getOutputStream();
+            DataOutputStream flowOut = new DataOutputStream(outStream);
+            switch (msgCode)
+            {
+            case 0:
+                flowOut.writeUTF(ADMON_MAIL + ",ALERTA EN " + nameDevice + " URGENTE,El dispositivo "
+                        + nameDevice + " acaba de presentar una falla en el sistema del tipo " + timeClass.MEMORY
+                        + ", se detecto con un uso de %" + memInfo + ", favor de tomar accion inmediatamente,1");
+                clientSocket.close();
+                break;
+            case 1:
+                flowOut.writeUTF(ADMON_MAIL + ",ALERTA EN " + nameDevice + " URGENTE,El dispositivo "
+                        + nameDevice + " acaba de presentar una falla en el sistema del tipo " + timeClass.DISK
+                        + ", se detecto con un uso de %" + diskInfo + ", favor de tomar accion inmediatamente,1");
+                clientSocket.close();
+                break;
+            case 2:
+                flowOut.writeUTF(ADMON_MAIL + ",ALERTA EN " + nameDevice + " URGENTE,El dispositivo "
+                        + nameDevice + " acaba de presentar una falla en el sistema del tipo " + timeClass.CONNECTION
+                        + ", se detecto con un uso de %" + connInfo + ", favor de tomar accion inmediatamente,1");
+                clientSocket.close();
+                break;
+            default:
+            }
+        }
+        catch (UnknownHostException e)
+        {
+            logger.error(
+                    "Ocurrio un error al intentar conectarse al host: [" + IP_MAIL + "] y puerto: [" + PORT_MAIL + "]");
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            logger.error(
+                    "Ocurrio un error al establecer el canal de datos: [" + IP_MAIL + "] y puerto [" + PORT_MAIL + "]");
+            e.printStackTrace();
+        }
+    }
+
+    private String getAsString(OID oid) throws IOException
     {
         ResponseEvent event = get(new OID[] {oid});
         return event.getResponse().get(0).getVariable().toString();
     }
 
-    public ResponseEvent get(OID oids[]) throws IOException
+    private ResponseEvent get(OID oids[]) throws IOException
     {
         PDU pdu = new PDU();
         for (OID oid : oids)
--- src/main/java/Bsnmp/TestMonitor.java
+++ src/main/java/Bsnmp/TestMonitor.java
@@ -3,30 +3,30 @@
  */
 package Bsnmp;
 
-import java.io.IOException;
-
 /**
  * @author Laptop
  *
  */
 public class TestMonitor
 {
+    static final String BD_ADDRESS = "127.0.0.1";
+    static final String BOLETAZO_ADDRESS = "25.6.157.184";
 
     /**
+     * Mandar a llamar las intancias necesarias para el monitoreo de los componentes
+     * existentes.
+     * 
      * @param args
      */
     public static void main(String[] args)
     {
-        try
-        {
-            Thread monitor = new Monitor();
-            monitor.start();
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-            System.out.println("Error IOException");
-        }
+        int cpuAlertPer = 80;
+        int diskAlertPer = 80;
+
+        Thread monitor = new Monitor("Base de datos", BOLETAZO_ADDRESS, 80, 80);
+        monitor.start();
+        // monitor = new Monitor("Boletazo server", BOLETAZO_ADDRESS, 80, 80);
+        // monitor.start();
     }
 
 }
