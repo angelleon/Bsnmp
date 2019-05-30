ff3e75c457272d7a0fb1b166a169501bcca843b7
Angel Leon <luianglenlop@gmail.com>
2019-05-30 01:51:06 -0500

    fixing misspelling

--- bmysql-snmp/bmysql-snmp.sh
+++ bmysql-snmp/bmysql-snmp.sh
@@ -4,7 +4,7 @@ mysql -h localhost -u boletazodev -pcontrapass -e exit Boletazo 1>/dev/null 2>&1
 
 if [ $? -eq 0 ]
 then 
-    echo successfull db connection
+    echo successful db connection
 else
     echo failed db connection
 fi
